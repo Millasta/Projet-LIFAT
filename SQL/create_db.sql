6eaeeb1e41fc94e755b284f97bebe02d374c210d
ThomChar <43478513+ThomChar@users.noreply.github.com>
2019-03-20 11:41:10 +0100

    Update create_db.sql

--- SQL/create_db.sql
+++ SQL/create_db.sql
@@ -1,17 +1,8 @@
+
+DROP DATABASE IF NOT EXISTS lifat_db;
 CREATE DATABASE IF NOT EXISTS lifat_db;
 
 USE lifat_db;
-
-DROP TABLE IF EXISTS equipes_projets.sql;
-DROP TABLE IF EXISTS equipes_responsables;
-DROP TABLE IF EXISTS encadrants_theses;
-DROP TABLE IF EXISTS dirigeants_theses;
-DROP TABLE IF EXISTS theses;
-DROP TABLE IF EXISTS equipes;
-DROP TABLE IF EXISTS dirigeants;
-DROP TABLE IF EXISTS encadrants;
-DROP TABLE IF EXISTS membres;
-DROP TABLE IF EXISTS lieu_travails;
 
 source lieu_travails.sql;
 source membres.sql;
